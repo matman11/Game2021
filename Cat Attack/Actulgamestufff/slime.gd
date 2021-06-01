a31a011e3d7161f1dfd7599c5c44b92818b9b35f
matman11 <matman11@kingshigh.school.nz>
2021-06-02 10:52:59 +1200

    latestslimemoveflip

---
+++ Cat Attack/Actulgamestufff/slime.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+var velocity = Vector2()
+export var direction = -1
+
+
+func _ready():
+	$AnimationPlayer.play("idle")
+
+	
+	
+func _physics_process(delta):
+	if is_on_wall():
+		direction = direction * -1
+		if direction == 1:
+			$Sprite.flip_h = true 
+		if direction == -1:
+			$Sprite.flip_h = false
+	
+	velocity.y += 20
+	
+	velocity.x = 50 * direction
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+
+
+	
+
+	
+	
+	
+
