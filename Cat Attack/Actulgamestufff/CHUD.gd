94570b8947d44b07c0ca30990b54ce75ea616a0f
matman11 <matman11@kingshigh.school.nz>
2021-06-28 09:43:18 +1200

    basecoincount

--- Cat Attack/Actulgamestufff/CHUD.gd
+++ Cat Attack/Actulgamestufff/CHUD.gd
@@ -4,3 +4,8 @@ var coins = 0
 
 func _ready():
 	$CoinCounter.text = String(coins)
+
+
+func _on_coin_coin_collected():
+	coins = coins + 1
+	_ready()
--- Cat Attack/Actulgamestufff/CoinAn.gd
+++ Cat Attack/Actulgamestufff/CoinAn.gd
@@ -3,12 +3,18 @@ extends Area2D
 func _ready():
 	$AnimationPlayer.play("coin1")
 
+signal coin_collected
 
 func _on_coin_body_entered(body):
 	if body.name == ("Player"):
-			$AnimationPlayer.play("Ctaken")
-			yield($AnimationPlayer, "animation_finished")
-			queue_free()
+		emit_signal("coin_collected")
+		$AnimationPlayer.play("Ctaken")
+		yield($AnimationPlayer, "animation_finished")
+
+			
+
+
+
 
 
 
